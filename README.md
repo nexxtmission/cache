90b60f937005f10237203f57917e86b03a09c94f
Saul <oliverasaul9201@gmail.com>
2023-02-14 20:08:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+## How to use LRUCache
+
+```javascript
+import LRUCache, { IStorage } from './index'
+
+class InMemoryStorage implements IStorage {
+    private __data: Record<string, string | undefined>;
+    constructor(){
+        this.__data = {};
+    }
+    delete(key: string): boolean {
+        return delete this.__data[key];
+    }
+    get(key: string): string | undefined {
+        return this.__data[key];
+    }
+    set(key: string, value: string): void {
+        this.__data[key]=value;
+    }
+}
+
+(async () => {
+    const cacheStorage = new InMemoryStorage();
+    const cache = new LRUCache<string>(cacheStorage, 5);
+
+    await cache.set('1', 'Hello1'); // the item 1 is added and is set as top 
+    await cache.set('2', 'Hello2'); // the item 2 is added and is set as top
+    await cache.set('3', 'Hello3'); // the item 3 is added and is set as top
+    await cache.set('4', 'Hello4'); // the item 4 is added and is set as top
+    await cache.set('5', 'Hello5'); // the item 5 is added and is set as top
+    await cache.set('6', 'Hello6'); // the item 6 is added and is set as top, the item 1 is deleted
+    await cache.get('2'); // the item 2 is used and then is set as top
+    await cache.set('7', 'Hello7'); // the item 7 is added and is set as top the item 3 is deleted
+    // await cache.print();
+    const array = [];
+    for await (const [key, value] of cache){
+        array.push(key);
+    }
+    console.log(
+        array.reduce(
+            ((acc: string, current: string) => {
+                return `${acc} > ${current}`
+            }) as any
+        )
+    )
+})()
+
+
+```
+
+
+## How to use memoize with cache
+
+```javascript
+import LRUCache, { IStorage, memoize } from './index'
+
+class InMemoryStorage implements IStorage {
+    private __data: Record<string, string | undefined>;
+    constructor(){
+        this.__data = {};
+    }
+    delete(key: string): boolean {
+        return delete this.__data[key];
+    }
+    get(key: string): string | undefined {
+        return this.__data[key];
+    }
+    set(key: string, value: string): void {
+        this.__data[key]=value;
+    }
+}
+
+const sleep = (time: number)=>new Promise(resolve => setTimeout(resolve, time))
+
+const expensiveCalculation = async (arg1: number, arg2: number) => {
+    await sleep(2000);
+    return arg1 + arg2;
+}
+
+(async () => {    
+    const cacheStorage = new InMemoryStorage();
+    const cache = new LRUCache<string>(cacheStorage, 3);
+    const expensiveCalculationMemoized = memoize({
+        func: expensiveCalculation,
+        cache,
+    });
+
+    console.log(await expensiveCalculationMemoized(1, 2)); // calling expensiveCalculation and caching the result
+    console.log(await expensiveCalculationMemoized(1, 2)); // using cached result
+    console.log(await expensiveCalculationMemoized(1, 3)); // calling expensiveCalculation and caching the result
+    console.log(await expensiveCalculationMemoized(1, 3)); // using cached result
+    console.log(await expensiveCalculationMemoized(1, 4)); // calling expensiveCalculation and caching the result
+    console.log(await expensiveCalculationMemoized(1, 4)); // using cached result
+    console.log(await expensiveCalculationMemoized(1, 5)); // calling expensiveCalculation and caching the result, key [1, 2] is removed from cache
+    console.log(await expensiveCalculationMemoized(1, 5)); // using cached result
+    console.log(await expensiveCalculationMemoized(1, 2)); // calling expensiveCalculation and caching the result again
+    console.log(await expensiveCalculationMemoized(1, 2)); // using cached result
+})()
+
+```
---
+++ index.ts
@@ -0,0 +1,201 @@
+export interface IStorage {
+    get(key: string): string | undefined | Promise<string | undefined>;
+    delete(key: string): boolean | Promise<boolean>;
+    set(key: string, value: string): void | Promise<void>;
+}
+
+interface INode<V> {
+    next?: string;
+    prev?: string;
+    value: V;
+    key: string;
+}
+
+export function memoize({
+    func,
+    keyResolver,
+    cache
+}: any) {
+    if (
+        typeof func != 'function' ||
+        (keyResolver != null && typeof keyResolver != 'function')
+    ) {
+        throw new TypeError('Expected a function');
+    }
+    if ((cache != null && (typeof cache.get != 'function' || typeof cache.set != 'function'))) {
+        throw new TypeError('Expected cache');
+
+    }
+    const memoized: any = async function (...args: any[]) {
+        const key = keyResolver ? keyResolver(...args) : JSON.stringify(args);
+        const valueFromCache = await cache.get(key);
+        if (valueFromCache) {
+            return valueFromCache;
+        }
+        var result = await func(...args);
+        await cache.set(key, result);
+        return result;
+    };
+    return memoized;
+}
+
+export default class LRUCache<V> {
+    private __headKeyPointer = 'lru-cache-head-key-pointer';
+    private __tailKeyPointer = 'lru-cache-tail-key-pointer';
+    private __sizeKey = 'lru-cache-size';
+
+    constructor(private storage: IStorage, readonly capacity: number) { }
+    private async _getNode(key: string): Promise<INode<V> | undefined> {
+        const serializedNode = await this.storage.get(key);
+        if (serializedNode) {
+            return JSON.parse(serializedNode) as INode<V>;
+        }
+        return undefined;
+    }
+    private async _setNode(key: string, node: INode<V>): Promise<void> {
+        await this.storage.set(key, JSON.stringify(node));
+    }
+    private async _getTail(): Promise<INode<V> | undefined> {
+        const tailKey = await this.storage.get(this.__tailKeyPointer);
+        if (tailKey) {
+            return this._getNode(tailKey);
+        }
+        return undefined;
+    }
+    private async _setTail(newTailNode: INode<V>): Promise<void> {
+        const currentTailNode = await this._getTail();
+        if (currentTailNode && currentTailNode.key !== newTailNode.key) {
+            currentTailNode.prev = newTailNode.key;
+            await this._setNode(currentTailNode.key, currentTailNode);
+            newTailNode.next = currentTailNode?.key;
+            newTailNode.prev = undefined;
+            await this._setNode(newTailNode.key, newTailNode);
+            await this.storage.set(this.__tailKeyPointer, newTailNode.key);
+        }
+    }
+    private async _getHead(): Promise<INode<V> | undefined> {
+        const headKey = await this.storage.get(this.__headKeyPointer);
+        if (headKey) {
+            return this._getNode(headKey);
+        }
+        return undefined;
+    }
+    private async _initialize(newNode: INode<V>) {
+        await this.storage.set(this.__tailKeyPointer, newNode.key);
+        await this.storage.set(this.__headKeyPointer, newNode.key);
+        await this._setNode(newNode.key, newNode);
+        return;
+    }
+    private async _removeHead() {
+        const currentHead = await this._getHead();
+        if (currentHead && currentHead.prev) {
+            const newHead = await this._getNode(currentHead.prev);
+            if (newHead) {
+                await this._setNode(newHead?.key, {
+                    ...newHead,
+                    next: undefined,
+                });
+                await this.storage.set(this.__headKeyPointer, newHead.key);
+            }
+        }
+        currentHead?.key && await this.storage.delete(currentHead.key);
+    }
+
+    public async _getSize(): Promise<number> {
+        return Number(this.storage.get(this.__sizeKey)) || 0;
+    }
+    private async _increaseSize(): Promise<number> {
+        const currentSize = await this._getSize();
+        const newSize = currentSize + 1;
+        await this.storage.set(this.__sizeKey, JSON.stringify(newSize));
+        return newSize;
+    }
+    private async _decreaseSize(): Promise<number> {
+        const currentSize = await this._getSize();
+        const newSize = (currentSize - 1) || 0;
+        await this.storage.set(this.__sizeKey, JSON.stringify(newSize));
+        return newSize;
+    }
+    private async __setAsTop(node: INode<V>): Promise<void> {
+        const previousNode = node.prev ? await this._getNode(node.prev) : undefined;
+        const nextNode = node.next ? await this._getNode(node.next) : undefined;
+        if (previousNode) {
+            previousNode.next = node.next;
+            await this._setNode(previousNode.key, previousNode);
+        }
+        if (nextNode) {
+            nextNode.prev = node.prev;
+            await this._setNode(nextNode.key, nextNode);
+        }
+        if (!nextNode && previousNode) {
+            await this.storage.set(this.__headKeyPointer, previousNode.key);
+        }
+
+        await this._setTail(node);
+    }
+    async get(key: string): Promise<V | undefined> {
+        const node = await this._getNode(key);
+        if (node) {
+            const tailKey = await this.storage.get(this.__tailKeyPointer);
+            if (node.key !== tailKey) {
+                await this.__setAsTop(node);
+            }
+            return node.value;
+        }
+        return undefined;
+    }
+    async set(key: string, value: V): Promise<void> {
+        const currentNode = await this._getNode(key);
+        if (currentNode !== undefined) {
+            await this._setNode(key, {
+                ...currentNode,
+                value,
+            });
+            await this.get(key);
+            return;
+        }
+        const newNode = {
+            value,
+            key,
+        };
+        const isFirstElement = !(await this._getSize());
+        if (isFirstElement) {
+            await this._initialize(newNode);
+            await this._increaseSize();
+            return;
+        }
+        await this._setTail(newNode);
+        const storageSize = await this._getSize();
+        if (storageSize + 1 > this.capacity) {
+            await this._removeHead();
+            return;
+        }
+        await this._increaseSize();
+    }
+    async *entries() {
+        let currentNode = await this._getTail();
+        yield [currentNode?.key, currentNode?.value];
+        while (currentNode?.next) {
+            currentNode = await this._getNode(currentNode.next);
+            yield [currentNode?.key, currentNode?.value];
+        }
+    }
+    async *reverseEntries() {
+        let currentNode = await this._getHead();
+        yield [currentNode?.key, currentNode?.value];
+        while (currentNode?.prev) {
+            currentNode = await this._getNode(currentNode.prev);
+            yield [currentNode?.key, currentNode?.value];
+        }
+    }
+    [Symbol.asyncIterator]() {
+        return this.entries();
+    }
+    async print(reverse = false) {
+        console.log('tail', '-->', await this.storage.get(this.__tailKeyPointer));
+        console.log('head', '-->', await this.storage.get(this.__headKeyPointer));
+        for await (const [key, value] of reverse ? this.reverseEntries() : this.entries()) {
+            console.log(key, '-->', value);
+        }
+    }
+}
